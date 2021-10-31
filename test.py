de0f4ecd9142dcccca894f42a4f8cda4aa675c86
unknown <utilities.lit1t@aleeas.com>
2021-10-31 12:49:10 +0530

    created the python pyramid maker

---
+++ test.py
@@ -0,0 +1,55 @@
+height = input("height(1, 5) ? ")
+
+
+if height == 1:
+    print('''
+#
+''')
+
+if height == 2:
+    print('''
+#
+##
+''')
+
+if height == 3:
+    print('''
+#
+##
+###
+''')
+
+if height == 4:
+    print('''
+#
+##
+###
+####
+''')
+
+if height == 5:
+    print('''
+#
+##
+###
+####
+#####
+''')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(hash)
